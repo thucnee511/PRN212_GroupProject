fbecdc8a585732773e45c4f09c470c1d54e82559
neru <ngocngocthuc@gmail.com>
2024-11-27 14:00:15 +0700

    feat: implement category repository

---
+++ Repositories/Repositories/Implements/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using Repositories.Entities;
+using Repositories.Enums;
+using Repositories.Repositories.Interfaces;
+using Repositories.UnitOfWork;
+
+namespace Repositories.Repositories.Implements
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IUnitOfWork<TheCoffeeStoreContext> unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public override void Delete(object id)
+        {
+            Category entity = GetById(id);
+            ArgumentNullException.ThrowIfNull(entity);
+            entity.Status = CategoryStatus.REMOVED;
+            Update(entity);
+        }
+
+        public override void Insert(Category entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+            Guid guid = Guid.NewGuid();
+            entity.Id = guid.ToString();
+            Entities.Add(entity);
+        }
+
+        public override void Update(Category entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+            entity.UpdatedAt = DateTime.UtcNow;
+            Entities.Update(entity);
+        }
+    }
+}
---
+++ Repositories/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Repositories.Entities;
+
+namespace Repositories.Repositories.Interfaces
+{
+    public interface ICategoryRepository : IGenericRepository<Category>, IDisposable
+    {
+    }
+}
