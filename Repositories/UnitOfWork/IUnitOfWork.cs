6f78b8de4bea6c32664e0a6694f14e822a9a985a
neru <ngocngocthuc@gmail.com>
2024-11-27 12:55:57 +0700

    feat: implement unit of work

--- Repositories/UnitOfWork/IUnitOfWork.cs
+++ Repositories/UnitOfWork/IUnitOfWork.cs
@@ -2,12 +2,23 @@
 
 namespace Repositories.UnitOfWork
 {
-    public interface IUnitOfWork<out TContext> where TContext : DbContext, new()
+    public interface IUnitOfWork<out TContext> : IDisposable where TContext : DbContext, new()
     {
-        TContext Context { get; }
+        /// <summary>
+        /// Start a transaction 
+        /// </summary>
         void Begin();
+        /// <summary>
+        /// Commit the transaction
+        /// </summary>
         void Commit();
+        /// <summary>
+        /// Rollback the transaction
+        /// </summary>
         void Rollback();
+        /// <summary>
+        /// Save the changes
+        /// </summary>
         void Save();
     }
 }
---
+++ Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Repositories.Entities;
+
+namespace Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork<TheCoffeeStoreContext>
+    {
+        private bool isDisposed;
+        private TheCoffeeStoreContext? context;
+        private IDbContextTransaction? transaction;
+        private TheCoffeeStoreContext Context
+        {
+            get
+            {
+                return context ??= new();
+            }
+        }
+
+        public UnitOfWork()
+        {
+            isDisposed = false;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing && context != null)
+                {
+                    context.Dispose();
+                    context = null;
+                }
+            }
+            isDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Begin()
+        {
+            transaction = Context.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            if (transaction != null)
+            {
+                transaction.Commit();
+                transaction.Dispose();
+                transaction = null;
+            }
+            else throw new Exception("Transaction is not started");
+        }
+
+        public void Rollback()
+        {
+            if (transaction != null)
+            {
+                transaction.Rollback();
+                transaction.Dispose();
+                transaction = null;
+            }
+            else throw new Exception("Transaction is not started");
+        }
+
+        public void Save()
+        {
+            try
+            {
+                Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error saving changes: {ex.Message}", ex);
+            }
+            finally
+            {
+                Rollback();
+            }
+        }
+    }
+}
